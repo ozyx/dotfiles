9b1675cbfe24b110833ba39dccde625276928067
ozyx <jessemazzella@gmail.com>
2018-07-29 17:53:35 -0700

    show all file extensions

--- macos/configure.sh
+++ macos/configure.sh
@@ -8,14 +8,17 @@
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 2
+
 # Always open everything in Finder's list view.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Show the hidden ~/Library folder.
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 2
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Save screencaps to ~/Pictures/Screenshots instead of Desktop
 defaults write com.apple.screencapture location ~/Pictures/Screenshots
